71a3c1061c8ef6606bb59ec98679c2a2f0665a15
annaxkotova <annaxkotova@gmail.com>
2021-07-22 11:16:58 +0200

    exercice 1 résolue

---
+++ Exercice 1/script.js
@@ -0,0 +1,14 @@
+function pluie() {
+    let rainy = true;
+
+if (rainy === true) {
+    console.log('« Bonjour, il ne pleut pas aujourd’hui, bonne journée. »'); //1er condition
+}
+else {
+    console.log('« Bonjour, il pleut aujourd’hui, vous devriez prendre votre parapluie. »');
+  }
+}
+
+//console.log peut diriger avec une seule variable
+
+pluie(); //appel de la fonction
