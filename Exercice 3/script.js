e09ce59a8f701d8282ae68af7de1ef57fe5ab567
AnnaxKotova <annaxkotova@gmail.com>
2021-07-25 01:03:40 +0200

    insertion condition oiseaux!==number

--- Exercice 3/script.js
+++ Exercice 3/script.js
@@ -1,41 +1,49 @@
 function birds(){
 let oiseaux;
 if (oiseaux){
-if (document.getElementById('poulet').checked) {
- oiseaux = document.getElementById('poulet').value;
-    if (document.getElementById('vitesse').value < 14){
-        document.getElementById("message").innerHTML = '« La vitesse saisie est trop petite »';
-    }
-    else if (document.getElementById('vitesse').value > 14){
-        document.getElementById("message").innerHTML = '« La vitesse saisie est trop grande »';
-    }
-    else{
-        document.getElementById("message").innerHTML = '« La vitesse saisie est correcte»';
-    }
-}
-
-
-else if (document.getElementById('canard').checked) {
-    oiseaux = document.getElementById('canard').value;
-        if (document.getElementById('vitesse').value < 60){
-            document.getElementById("message").innerHTML = '« La vitesse saisie est trop petite »';
-        }
-        else if (document.getElementById('vitesse').value > 60){
-            document.getElementById("message").innerHTML = '« La vitesse saisie est trop grande »';
-        }
-        else{
-            document.getElementById("message").innerHTML = '« La vitesse saisie est correcte»';
+    if (document.getElementById('poulet').checked){
+        oiseaux = document.getElementById('poulet').value;
+        if (typeof oiseaux !== "number"){
+            document.getElementById("message").innerHTML = '« Veuillez saisir la vitesse dans un format correct»';
+            }
+        else {
+            if (document.getElementById('vitesse').value < 14){
+                document.getElementById("message").innerHTML = '« La vitesse saisie est trop petite »';
+            }
+            else if (document.getElementById('vitesse').value > 14){
+                document.getElementById("message").innerHTML = '« La vitesse saisie est trop grande »';
+            }
+            else{
+                document.getElementById("message").innerHTML = '« La vitesse saisie est correcte»';
+            }
         }
     }
-
-else {
-    document.getElementById("message").innerHTML = '« Veuillez choisir une oiseaux»';
-}
+    else if (document.getElementById('canard').checked) {
+        oiseaux = document.getElementById('canard').value;
+        if (typeof oiseaux === "number"){
+            if (document.getElementById('vitesse').value < 60){
+                document.getElementById("message").innerHTML = '« La vitesse saisie est trop petite »';
+            }
+            else if (document.getElementById('vitesse').value > 60){
+                document.getElementById("message").innerHTML = '« La vitesse saisie est trop grande »';
+            }
+            else{
+                document.getElementById("message").innerHTML = '« La vitesse saisie est correcte»';
+            }
+        }
+        else {
+            document.getElementById("message").innerHTML = '« Veuillez saisir la vitesse dans un format correct»';
+        }
+    }
+    else {
+        document.getElementById("message").innerHTML = '« Veuillez choisir une oiseaux»';
+    }
 }
 
 else {
     document.getElementById("message").innerHTML = '« Veuillez saisir la vitesse »';
-}
+    }
 }
 
 document.getElementById("envoyer").addEventListener("click", birds);
+
